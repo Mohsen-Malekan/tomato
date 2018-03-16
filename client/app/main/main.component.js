b2eb84d3388b2e4814bb1bc819e2e3be8e1c3905
Mohsen-Malekan <mhs.malekan@gmail.com>
2018-03-16 16:29:30 +0330

    certs and refactors and minor changes in admin and client

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,45 +3,27 @@ import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
 
 export class MainController {
-  awesomeThings = [];
-  newThing = '';
 
   /*@ngInject*/
-  constructor($http, $scope, socket) {
-    /*this.$http = $http;
-    this.socket = socket;
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });*/
+  constructor (Auth, $state) {
+    this.$state = $state;
+    this.Auth   = Auth;
   }
 
-  $onInit() {
-    /*this.$http.get('/api/things')
-      .then(response => {
-        this.awesomeThings = response.data;
-        this.socket.syncUpdates('thing', this.awesomeThings);
-      });*/
-  }
-
-  addThing() {
-    /*if(this.newThing) {
-      this.$http.post('/api/things', {
-        name: this.newThing
-      });
-      this.newThing = '';
-    }*/
-  }
-
-  deleteThing(thing) {
-    this.$http.delete(`/api/things/${thing._id}`);
+  $onInit () {
+    if (this.Auth.hasRoleSync('admin')) {
+      return this.$state.go('admin');
+    }
+    else {
+      return this.$state.go('user');
+    }
   }
 }
 
 export default angular.module('tomatoApp.main', [uiRouter])
   .config(routing)
   .component('main', {
-    template: require('./main.html'),
-    controller: MainController
+    template   : require('./main.html'),
+    controller : MainController
   })
   .name;
